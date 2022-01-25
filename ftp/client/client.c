6ef31aeea28626922f5f93c7ca6707e8ae14e8cb
jbottelb <jbottelb@nd.edu>
2022-01-25 14:33:08 -0500

    inital setup

--- ftp/client/client.c
+++ ftp/client/client.c
@@ -2,7 +2,13 @@
 
 int main()
 {
+    // get from CL the hostname and Filename
 
+    // connect to host socket
+
+    // request file
+
+    // save file somewhere with like a filestream or something
     printf("Client Code\n");
     
 
--- ftp/server/server.cpp
+++ ftp/server/server.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 
+//./proj1server 10584
+
 int main(){
     printf("This is the server\n");
     std::cout << "The server is working" << std::endl;
+    // listen on specfiied port, for each connection, serve the requested file
+
     return 0;
 }
