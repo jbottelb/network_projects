de98f2d9ac433ca5a64e34f1565c8df8dc04dbb5
jbottelb <jbottelb@nd.edu>
2022-04-21 22:39:23 -0400

    added Json

--- wordle/test.c
+++ wordle/test.c
@@ -15,8 +15,16 @@ int main() {
     sscanf("[{ key : value }]", "[{ %s : %s }]", key, value);
     printf("%s %s\n", key, value);
     char Json[BUFSIZ] = "{ \"key\" : \"value\"}";
+
+    cJSON *v1 = cJSON_CreateString("A");
     cJSON *json = cJSON_Parse(Json);
+    cJSON_AddItemToObject(json, "1", v1);
+    cJSON_AddStringToObject(json, "2", "B");
     printf("%s\n", cJSON_Print(json));
+
+    printf("This should be B : %s\n", cJSON_GetObjectItemCaseSensitive(json, "2")->valuestring);
+
+    cJSON_Delete(json);
 
     return 0;
 }
