3e06da3f3ef790436139fad32735565c2856b7b2
jbottelb <jbottelb@nd.edu>
2022-04-26 18:48:17 -0400

    reworked server

--- wordle/client/client.py
+++ wordle/client/client.py
@@ -22,28 +22,62 @@ def main():
         exit(1)
 
 def hacky_recv(sock):
-    r = {}
-    rec = b''
     while True:
-        rec += sock.recv(128)
-        try:
-            r = json.loads(rec.decode())
-            print(r)
-            if (r["MessageType"] == "Chat"):
+        r = {}
+        rec = b''
+        while True:
+            rec += sock.recv(128)
+            try:
+                r = json.loads(rec.decode())
                 break
-            return r
-        except:
-            continue
-    print(r["Data"]["Name"] + ": " + r["Data"]["Text"])
-    hacky_recv(sock)
-    print("Reciver function exiting!")
+            except:
+                continue
+        if r["MessageType"] == "Chat":
+            print(r["Data"]["Name"] + ": " + r["Data"]["Text"])
+        elif r["MessageType"] == "PromptForGuess":
+            print("The server prompts for a guess")
+        elif r["MessageType"] == "StartRound":
+            print("The round has started")
+        elif r["MessageType"] == "StartGame":
+            print("The Game has started")
+        elif r["MessageType"] == "JoinInstanceResult":
+            if r["Data"]["Result"] == "yes":
+                print("Entered Lobby")
+            else:
+                print("Failed to enter Lobby")
+        elif r["MessageType"] == "GuessResponse":
+            if r["Data"]["Accepted"] == "yes":
+                print("Guess accepted")
+            else:
+                print("Word not in list. Guess again")
+        elif r["MessageType"] == "StartInstance":
+            exit(0)
+        elif r["MessageType"] == "EndRound":
+            print("Round recap")
+            for player in r["Data"]["PlayerInfo"]:
+                print(player["Name"] + ": " + str(player["Score"]))
+        elif r["MessageType"] == "EndGame":
+            print("The game is over!")
+            print("Winner: " + r["Data"]["WinnerName"])
+            exit(0)
+        elif r["MessageType"] == "GuessResult":
+            dat = r["Data"]["PlayerInfo"]
+            for e in dat:
+                if e["Name"] == NAME:
+                    print("Reponse: " + e["Result"])
+        else:
+            # anything we havent coded
+            print(r)
+
 
 def chat_handler(sock):
     print("Waiting for server, feel free to chat")
-    print("After server response, exit with Ctrl-D")
+    print("After server response, type exit")
     m = {}
     args = {}
     for line in sys.stdin:
+        if "exit" in line:
+            exit(0)
         m["MessageType"] = "Chat"
         args["Name"] = NAME
         args["Text"] = line.strip()
@@ -53,88 +87,41 @@ def chat_handler(sock):
 
 def send_request(req):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        # spawn a listener, which accepts and interprets all responses
+        sock.connect((req["Data"]["Server"], int(req["Data"]["Port"])))
+        sock.sendall(json.dumps(req).encode())
+        listener = threading.Thread(target=hacky_recv, args=(sock,))
+        listener.start()
         if req["MessageType"] == "Join":
-            sock.connect((req["Data"]["Server"], int(req["Data"]["Port"])))
-            sock.sendall(json.dumps(req).encode())
-
-            join = hacky_recv(sock)
-            print(join)
-
-            if join["Data"]["Result"] == "no":
-                exit(0)
-            else:
-                x = threading.Thread(target=chat_handler, args=(sock,))
-                x.start()
-                join = hacky_recv(sock)
-                print(join)
-                x.join()
-                sys.exit(0)
+            x = threading.Thread(target=chat_handler, args=(sock,))
+            x.start()
+            x.join()
+            sys.exit(0)
         elif req["MessageType"] == "JoinInstance":
-            sock.connect((req["Data"]["Server"], int(req["Data"]["Port"])))
-            sock.sendall(json.dumps(req).encode())
-
-            join = hacky_recv(sock)
-            print(join)
-
-            if join["Data"]["Result"] == "no":
-                exit(0)
-            else:
-                # Start Game message
-                result = hacky_recv(sock)
-                print(result)
-
-                for _ in range(int(result["Data"]["Rounds"])):
-                    # Start Round
-                    result = hacky_recv(sock)
-                    print(result)
-
-                    # Prompt for Guess
-                    result = hacky_recv(sock)
-                    print(result)
-                    flag = "again"
-                    # Guess logic
-                    while flag == "again":
-                        # Build and send Guess
+            while True:
+                print("Enter Chat or Guess <guess string>")
+                for line in sys.stdin:
+                    try:
                         guess, args = {}, {}
-                        listener = threading.Thread(target=hacky_recv, args=(sock,))
-                        listener.start()
-                        for line in sys.stdin:
-                            line = line.split()
-                            guess["MessageType"] = line[0]
-                            if (guess["MessageType"] == "Chat"):
-                                m = {}
-                                message = input()
-                                m["MessageType"] = "Chat"
-                                args["Name"] = NAME
-                                args["Text"] = message.strip()
-                                m["Data"] = args
-                                print(m)
-                                sock.sendall(json.dumps(m).encode())
-                                flag = "again"
-                            else:
-                                args["Name"] = NAME
-                                args["Guess"] = line[1]
-                                guess["Data"] = args
-                                print(guess)
-                                sock.sendall(json.dumps(guess).encode())
-
-                                # Guess Response
-                                result = hacky_recv(sock)
-                                print(result)
-                                break
-
-
-                    win = False
-                    for p in result["Data"]["PlayerInfo"]:
-                        if p["Winner"] == "yes":
-                            print("Game won by", p["Name"])
-                            win = True
-
-                    if win:
-                        break
-                # End Game
-                result = hacky_recv(sock)
-                print(result)
+                        line = line.split()
+                        guess["MessageType"] = line[0]
+                        if (guess["MessageType"] == "Chat"):
+                            print("Type your message")
+                            m = {}
+                            message = input()
+                            m["MessageType"] = "Chat"
+                            args["Name"] = NAME
+                            args["Text"] = message.strip()
+                            m["Data"] = args
+                            sock.sendall(json.dumps(m).encode())
+                            flag = "again"
+                        else:
+                            args["Name"] = NAME
+                            args["Guess"] = line[1]
+                            guess["Data"] = args
+                            sock.sendall(json.dumps(guess).encode())
+                    except:
+                        print("Issue with input, try again")
         else:
             print("Something went wrong if we got here")
             exit(1)
